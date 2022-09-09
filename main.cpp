47595b757692845b772606fe89a33851e3d7179e
Syadon <bokagabor4@gmail.com>
2022-09-09 18:16:47 +0200

    errors found

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //const variable name typo
+    std::cout << '1-100 ertekek duplazasa' //missing semicolon, optionally a new line and ' instead of "
+    for (int i = 0;) //incomplete for syntax
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //no break statement to exit for loop
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek:" //unfinished code to output array elements
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //atlag is not inicialized
+    for (int i = 0; i < N_ELEMENTS, i++) //comma instead of semicolon
     {
-        atlag += b[i]
+        atlag += b[i] //missing semicolon
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
